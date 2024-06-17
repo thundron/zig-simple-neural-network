9ae2c65f02e488c072a584cfa88a3fe88e50cfc8
Lorenzo Alberto Maria Ambrosi <la@thundron.dev>
2024-06-17 15:03:15 +0200

    First draft, calculate with random weights
    Signed-off-by: Lorenzo Alberto Maria Ambrosi <la@thundron.dev>

---
+++ src/main.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const input_layers = [_]f64{ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1 };
+
+    const output_layer = [_]f64{ 0, 1, 1, 0 };
+    var weights: [3]f64 = undefined;
+
+    const rand = std.crypto.random;
+    for (&weights) |*w| {
+        w.* = 2 * rand.float(f64) - 1;
+    }
+
+    var iteration: u32 = 0;
+    while (iteration < 10_000) : (iteration += 1) {
+        var output: f64 = 0;
+        for (input_layers) |input| {
+            output += input * weights[iteration % 3];
+        }
+
+        output = 1 / (1 + std.math.exp(-output));
+        for (&weights, 0..) |*w, i| {
+            w.* += input_layers[i] * (output_layer[0] - output) * output * (1 - output);
+        }
+    }
+
+    const input: [3]f64 = .{ 1, 0, 0 };
+    var result: f64 = 0;
+    for (input, 0..) |x, i| {
+        result += x * weights[i];
+    }
+    result = 1 / (1 + std.math.exp(-result));
+
+    std.debug.print("{d}\n", .{result});
+}
